2df76ece5f4cb597dc792f3b3ccf208d2240da34
Abramov Nikita <nikita.ab@livetex.ru>
2017-04-17 16:14:58 +0300

    refactor unit

--- src/app/models/unit.ts
+++ src/app/models/unit.ts
@@ -1,4 +1,4 @@
-import { generateId } from '../../utils';
+import { utils } from '../../utils';
 import { Vector } from './math-models/vector';
 import { Sprite } from './animation/sprite';
 import { direction } from './math-models/direction';
@@ -12,7 +12,15 @@ export type unitOptions = {
   min_speed?: number
   accelerate_module?: number;
 }
-
+let defaulUnitOptions: unitOptions = {
+  sprite: null,
+  name: '',
+  position: new Vector(0, 0),
+  accelerate_module: 0,
+  max_speed: 0,
+  min_speed: 0,
+  size: new Vector(50, 50)
+};
 export class Unit {
   private id: string;
   private name: string;
@@ -27,17 +35,83 @@ export class Unit {
   private accelerate_module: number;
 
   constructor(options: unitOptions) {
-    this.id = generateId();
-    this.name = options.name || '';
-    this.sprite = options.sprite;
-    this.setPosition(options.position || new Vector(0, 0));
+    let mergedOptions = utils.merge([options, defaulUnitOptions]);
+    this.setId(utils.generateId());
+    this.setName(mergedOptions.name);
+    this.setSprite(mergedOptions.sprite);
+    this.setPosition(mergedOptions.position);
+    this.setAccelerateModule(mergedOptions.accelerate_module);
+    this.setSize(mergedOptions.size);
+    this.setMaxSpeed(mergedOptions.max_speed);
+    this.setMinSpeed(mergedOptions.min_speed);
+
     this.setAccelerate(new Vector(0, 0));
-    this.accelerate_module = options.accelerate_module || 0;
-    this.setSize(options.size || new Vector(50, 50));
-    this.max_speed = options.max_speed || 0;
-    this.min_speed = options.min_speed || 0;
     this.setDirection(direction.RIGHT);
     this.setSpeed(this.getDirection());
+  }
+  // public deleteUnit (callback: Function): void {
+  //   callback();
+  //   delete this;
+  // }
+
+  public update(deltaTime: number): void {
+    this.setPosition(this.getNewPosition(deltaTime));
+    this.setSpeed(this.getSpeed().add(this.getAccelerate().multiply(deltaTime)));
+    this.setSpeed(
+      this.getSpeed().length() < this.getMaxSpeed() ? this.getSpeed() : this.getSpeed().setLength(this.getMaxSpeed())
+    );
+    this.sprite.update(deltaTime);
+  }
+
+  public getNewPosition(deltaTime: number): Vector {
+    return this.getPosition().add(this.getSpeed().multiply(deltaTime));
+  }
+
+  public render(context: CanvasRenderingContext2D): void {
+    this.sprite.render(context, this.getDrawPoint(), this.getSize());
+  }
+
+  public rotate(direction: Vector): void {
+    if (this.getDirection() === direction) {
+      this.forward();
+    } else {
+      if (this.getDirection().dot(direction)) {
+        this.back();
+      } else {
+        this.setDirection(direction);
+        this.setSpeed(this.getDirection().setLength(this.getMinSpeed()));
+      }
+    }
+  }
+
+  public rotateLeft(): void {
+    this.setDirection(new Vector(this.getDirection().y, -this.getDirection().x));
+    this.setSpeed(this.getDirection().setLength(this.getMinSpeed()));
+  }
+
+  public rotateRight(): void {
+    this.setDirection(new Vector(-this.getDirection().y, this.getDirection().x));
+    this.setSpeed(this.getDirection().setLength(this.getMinSpeed()));
+  }
+
+  public forward(): void {
+    this.setAccelerate(this.getDirection().multiply(this.getAccelerateModule()));
+    this.setSpeed(
+      this.getSpeed().isNullVector() || this.getSpeed().dot(this.getDirection()) < 0 ?
+        this.getDirection() :
+        this.getSpeed());
+  }
+
+  public back(): void {
+    this.setAccelerate(this.getDirection().multiply(-this.getAccelerateModule()));
+    if (this.getSpeed().isNullVector()) {
+      this.setSpeed(this.getDirection().multiply(-1));
+    }
+  }
+
+  public stop(): void {
+    this.setSpeed(new Vector(0, 0));
+    this.setAccelerate(new Vector(0, 0));
   }
 
   public getId(): string {
@@ -64,73 +138,8 @@ export class Unit {
     return new Vector(-this.getSize().x / 2, -this.getSize().y / 2);
   }
 
-  // public deleteUnit (callback: Function): void {
-  //   callback();
-  //   delete this;
-  // }
-
-  public update(deltaTime: number): void {
-    this.setPosition(this.getNewPosition(deltaTime));
-    this.setSpeed(this.getSpeed().add(this.getAccelerate().multiply(deltaTime)));
-    this.setSpeed(
-      this.getSpeed().length() < this.max_speed ? this.getSpeed() : this.getSpeed().setLength(this.max_speed)
-    );
-    this.sprite.update(deltaTime);
-  }
-
-  public getNewPosition(deltaTime: number): Vector {
-    return this.getPosition().add(this.getSpeed().multiply(deltaTime));
-  }
-
-  public render(context: CanvasRenderingContext2D): void {
-    this.sprite.render(context, this.getDrawPoint(), this.getSize());
-  }
-
-  public rotate(direction: Vector): void {
-    if (this.getDirection() === direction) {
-      this.forward();
-    } else {
-      if (this.getDirection().dot(direction)) {
-        this.back();
-      } else {
-        this.setDirection(direction);
-        this.setSpeed(this.getDirection().setLength(this.min_speed));
-      }
-    }
-  }
-
-  public rotateLeft(): void {
-    this.setDirection(new Vector(this.getDirection().y, -this.getDirection().x));
-    this.setSpeed(this.getDirection().setLength(this.min_speed));
-  }
-
-  public rotateRight(): void {
-    this.setDirection(new Vector(-this.getDirection().y, this.getDirection().x));
-    this.setSpeed(this.getDirection().setLength(this.min_speed));
-  }
-
-  public forward(): void {
-    this.setAccelerate(this.getDirection().multiply(this.accelerate_module));
-    this.setSpeed(
-      this.getSpeed().isNullVector() || this.getSpeed().dot(this.getDirection()) < 0 ?
-        this.getDirection() :
-        this.getSpeed());
-  }
-
-  public back(): void {
-    this.setAccelerate(this.getDirection().multiply(-this.accelerate_module));
-    if (this.getSpeed().isNullVector()) {
-      this.setSpeed(this.getDirection().multiply(-1));
-    }
-  }
-
   public getDirection(): Vector {
     return this.direction;
-  }
-
-  public stop(): void {
-    this.setSpeed(new Vector(0, 0));
-    this.setAccelerate(new Vector(0, 0));
   }
 
   private setSpeed(speed: Vector): void {
@@ -155,5 +164,45 @@ export class Unit {
 
   private setSize(size: Vector): void {
     this.size = size;
+  }
+
+  private getMaxSpeed(): number {
+    return this.max_speed;
+  }
+
+  private setMaxSpeed(maxSpeed: number): void {
+    this.max_speed = maxSpeed;
+  }
+
+  private getMinSpeed(): number {
+    return this.min_speed;
+  }
+
+  private setMinSpeed(minSpeed: number): void {
+    this.min_speed = minSpeed;
+  }
+
+  private getSprite(): Sprite {
+    return this.sprite;
+  }
+
+  private setSprite(sprite: Sprite): void {
+    this.sprite = sprite;
+  }
+
+  private setId(id: string): void {
+    this.id = id;
+  }
+
+  private setName(name: string): void {
+    this.name = name;
+  }
+
+  private setAccelerateModule(accelerateModule: number): void {
+    this.accelerate_module = accelerateModule;
+  }
+
+  private getAccelerateModule(): number {
+    return this.accelerate_module;
   }
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,20 @@
-export function generateId(): string {
+export module utils {
+  'use strict';
+  export function generateId(): string {
     return '_' + Math.random().toString(36).substr(2, 9);
+  }
+
+  export function merge<T>(objects: Array<T>): T {
+    let result = {};
+
+    objects.map(object => {
+      Object.keys(object).map(key => {
+        if (object[key] !== null && object[key] !== 'undefined') {
+          result[key] = object[key];
+        }
+      });
+    });
+    return result as T;
+  }
 }
+
